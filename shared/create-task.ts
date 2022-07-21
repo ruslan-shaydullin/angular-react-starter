09f853de460922b96aceb6a00e4949a260cd2dd2
Ruslan Shaydullin <shaydullin.r.d@outlook.com>
2022-07-21 21:47:08 +0500

    Implement shared task creation

---
+++ shared/create-task.ts
@@ -0,0 +1,13 @@
+import { Workspace, Result, Task } from './types';
+import { validateTask } from './validation';
+import { success, failure, nextId } from './result';
+export function createTask(state:Workspace,input:unknown,now:string):Result<Workspace> {
+ const parsed=validateTask(input,state);
+ if(!parsed.ok) return failure(state,...parsed.errors);
+ if(!Number.isFinite(Date.parse(now))) return failure(state,'Creation time is invalid.');
+ const task:Task={...parsed.value,id:nextId('T',state.tasks),tags:[],dependsOn:[],checklist:[],comments:[],timeEntries:[],archived:false,createdAt:now,updatedAt:now};
+ return success({...state,tasks:[...state.tasks,task]});
+}
+export function blankTask(state:Workspace) {
+ return {title:'',description:'',status:'backlog',priority:'normal',projectId:state.projects[0]?.id||'',assignee:'',dueDate:'',estimate:0};
+}
