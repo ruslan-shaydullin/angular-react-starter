f1a45e464a67131f4bf08381b454729c612b11f8
Ruslan Shaydullin <shaydullin.r.d@outlook.com>
2022-08-23 10:44:50 +0500

    Build React navigation shell

--- react/src/app.js
+++ react/src/app.js
@@ -1,4 +1,2 @@
-import './workshop/workshop.css';
-export default function App() {
- return <><a className="skip-link" href="#main">Skip to workspace</a><div className="shell"><header className="masthead"><div><h1>Release workshop</h1><p>Plan, review, and ship a small release together.</p></div><span className="framework">React 18</span></header><main id="main"><section className="panel"><h2>Delivery workspace</h2><p>Explore the shared release planning examples.</p></section></main></div></>;
-}
+import './workshop/workshop.css';import {WorkshopProvider} from './workshop/store';import Shell from './workshop/Shell';
+export default function App(){return <WorkshopProvider><Shell /></WorkshopProvider>;}
