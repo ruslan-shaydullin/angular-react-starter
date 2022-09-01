b49c9fa14a22e150b7a1c598e87b7f12179e3735
Ruslan Shaydullin <shaydullin.r.d@outlook.com>
2022-09-01 20:21:46 +0500

    Build React responsive workspace composition

--- react/src/app.js
+++ react/src/app.js
@@ -1,2 +1,2 @@
-import './workshop/workshop.css';import {WorkshopProvider} from './workshop/store';import Shell from './workshop/Shell';
-export default function App(){return <WorkshopProvider><Shell /></WorkshopProvider>;}
+import './workshop/workshop.css';import {WorkshopProvider} from './workshop/store';import Shell from './workshop/Shell';import Workspace from './workshop/Workspace';import {WorkshopBoundary} from './workshop/EmptyState';
+export default function App(){return <WorkshopBoundary><WorkshopProvider><Shell><Workspace/></Shell></WorkshopProvider></WorkshopBoundary>;}
