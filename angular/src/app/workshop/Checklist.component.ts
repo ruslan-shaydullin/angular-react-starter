d4df5006fba1ee64b1267cfc1c03198d234ac268
Ruslan Shaydullin <shaydullin.r.d@outlook.com>
2022-09-06 22:48:12 +0500

    Build Angular checklist editor

---
+++ angular/src/app/workshop/Checklist.component.ts
@@ -0,0 +1,7 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { WorkshopStore } from './store.service';
+import {Task} from '../../workshop/shared/types';
+@Component({selector:'workshop-checklist',templateUrl:'./Checklist.component.html'})
+export class ChecklistComponent { constructor(public s:WorkshopStore) {}
+@Input() task!:Task;text='';get completed(){return this.task.checklist.filter(item=>item.done).length;}add():void{if(this.s.dispatch('checklist.add',{id:this.task.id,text:this.text}))this.text='';}
+}
