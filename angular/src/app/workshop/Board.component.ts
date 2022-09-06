fcbad9aa864b79f3560db9717fcd81d19a440456
Ruslan Shaydullin <shaydullin.r.d@outlook.com>
2022-09-06 21:15:55 +0500

    Build Angular status board

---
+++ angular/src/app/workshop/Board.component.ts
@@ -0,0 +1,7 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { WorkshopStore } from './store.service';
+import {boardColumns} from '../../workshop/shared/grouping';import {adjacentStatus,boardLimitWarning} from '../../workshop/shared/board';import {filterTasks} from '../../workshop/shared/filters';
+@Component({selector:'workshop-board',templateUrl:'./Board.component.html'})
+export class BoardComponent { constructor(public s:WorkshopStore) {}
+adjacentStatus=adjacentStatus;boardLimitWarning=boardLimitWarning;get columns(){return boardColumns(filterTasks(this.s.state,this.s.filters).filter(task=>this.s.state.preferences.showCompleted||task.status!=='done'));}
+}
