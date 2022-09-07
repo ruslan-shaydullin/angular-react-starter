738bd7e33b7679a4b06bdb07436fa302b23f0cd8
Ruslan Shaydullin <shaydullin.r.d@outlook.com>
2022-09-07 09:17:42 +0500

    Build Angular comment thread

---
+++ angular/src/app/workshop/Comments.component.ts
@@ -0,0 +1,7 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { WorkshopStore } from './store.service';
+import {Task} from '../../workshop/shared/types';import {memberName} from '../../workshop/shared/members';
+@Component({selector:'workshop-comments',templateUrl:'./Comments.component.html'})
+export class CommentsComponent { constructor(public s:WorkshopStore) {}
+@Input() task!:Task;text='';author=this.s.state.members[0]?.id||'';memberName=memberName;add():void{if(this.s.dispatch('comment.add',{id:this.task.id,text:this.text,author:this.author}))this.text='';}
+}
