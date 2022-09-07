4c38936afa94ddb95465b8d895f459b472869183
Ruslan Shaydullin <shaydullin.r.d@outlook.com>
2022-09-07 20:48:54 +0500

    Build Angular team directory

---
+++ angular/src/app/workshop/Team.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { WorkshopStore } from './store.service';
+
+@Component({selector:'workshop-team',templateUrl:'./Team.component.html'})
+export class TeamComponent { constructor(public s:WorkshopStore) {}
+
+draft={name:'',role:'',capacity:16};initials(name:string):string{return name.split(' ').map(part=>part[0]).join('');}
+add():void{if(this.s.dispatch('member.add',this.draft))this.draft={name:'',role:'',capacity:16};}
+
+}
