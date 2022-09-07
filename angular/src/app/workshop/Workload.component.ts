35ecd73ec4f237a6084b05c6d499ea8c9f8d7e25
Ruslan Shaydullin <shaydullin.r.d@outlook.com>
2022-09-07 21:10:55 +0500

    Build Angular workload report

---
+++ angular/src/app/workshop/Workload.component.ts
@@ -0,0 +1,7 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { WorkshopStore } from './store.service';
+import {memberWorkload,unassignedWork} from '../../workshop/shared/workload';
+@Component({selector:'workshop-workload',templateUrl:'./Workload.component.html'})
+export class WorkloadComponent { constructor(public s:WorkshopStore) {}
+get members(){return memberWorkload(this.s.state);}get unassigned(){return unassignedWork(this.s.state);}
+}
