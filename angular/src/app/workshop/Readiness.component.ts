c6278dbc59927117daecdb8451a71dac4ca9f4a7
Ruslan Shaydullin <shaydullin.r.d@outlook.com>
2022-09-08 10:44:12 +0500

    Build Angular release readiness

---
+++ angular/src/app/workshop/Readiness.component.ts
@@ -0,0 +1,7 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { WorkshopStore } from './store.service';
+import {releaseReadiness} from '../../workshop/shared/readiness';
+@Component({selector:'workshop-readiness',templateUrl:'./Readiness.component.html'})
+export class ReadinessComponent { constructor(public s:WorkshopStore) {}
+get releases(){return releaseReadiness(this.s.state);}
+}
