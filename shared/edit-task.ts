355ea5c708fb0af73728116049ecd7857792434d
Ruslan Shaydullin <shaydullin.r.d@outlook.com>
2022-09-12 09:55:00 +0500

    Test shared task editing preservation

--- shared/edit-task.ts
+++ shared/edit-task.ts
@@ -7,6 +7,7 @@ export function editTask(state:Workspace,id:string,input:unknown,now:string):Res
  if(task.archived) return failure(state,'Restore the task before editing it.');
  const parsed=validateTask({...task,...(isRecord(input)?input:{})},state);
  if(!parsed.ok) return failure(state,...parsed.errors);
+ if(parsed.value.projectId!==task.projectId && state.milestones.some(milestone=>milestone.taskIds.includes(id))) return failure(state,'Remove the task from its milestone before changing projects.');
  if(parsed.value.status!==task.status) return failure(state,'Use the status action to change workflow state.');
  if(!Number.isFinite(Date.parse(now))) return failure(state,'Update time is invalid.');
  return replaceTask(state,success<Task>({...task,...parsed.value,updatedAt:now}));
