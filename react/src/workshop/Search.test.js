20d8c20112f3fa1942a18942b92f0b162537e0b7
Ruslan Shaydullin <shaydullin.r.d@outlook.com>
2022-09-29 16:30:40 +0500

    Test React search and clear

---
+++ react/src/workshop/Search.test.js
@@ -0,0 +1,5 @@
+import {screen,fireEvent,within} from '@testing-library/react';
+import {renderWorkshop,storedWorkspace,changeRoute,openTask} from './testHelpers';
+
+test('search narrows the table and clear restores pagination',async()=>{const {user}=renderWorkshop();await user.type(screen.getByRole('searchbox'),'queue');expect(screen.getByRole('button',{name:'Add queue depth dashboard'})).toBeInTheDocument();expect(screen.getByText('Showing 1–1 of 1 tasks')).toBeInTheDocument();await user.click(screen.getByRole('button',{name:/Clear filters/}));expect(screen.getByText('Showing 1–6 of 12 tasks')).toBeInTheDocument();});
+test('empty search results include a recovery action',async()=>{const {user}=renderWorkshop();await user.type(screen.getByRole('searchbox'),'no matching release work');expect(screen.getByText(/No tasks match these filters/)).toBeInTheDocument();expect(screen.getByRole('button',{name:'Next page'})).toBeDisabled();});
---
+++ react/src/workshop/testHelpers.js
@@ -0,0 +1,8 @@
+import {render,screen,fireEvent} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../app';
+import {STORAGE_KEY} from './shared/persistence';
+export function renderWorkshop(hash='#/tasks'){window.localStorage.clear();window.history.replaceState(null,'',hash);const view=render(<App/>);return {...view,user:userEvent.setup()};}
+export function storedWorkspace(){return JSON.parse(window.localStorage.getItem(STORAGE_KEY)).workspace;}
+export function changeRoute(hash){window.history.replaceState(null,'',hash);fireEvent(window,new HashChangeEvent('hashchange'));}
+export async function openTask(user,title){await user.type(screen.getByRole('searchbox'),title);await user.click(screen.getByRole('button',{name:title,exact:true}));}
