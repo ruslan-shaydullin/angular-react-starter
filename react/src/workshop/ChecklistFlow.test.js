bd89e07de1dadefca646bac157e4d4325c98e117
Ruslan Shaydullin <shaydullin.r.d@outlook.com>
2022-09-30 20:43:45 +0500

    Test React checklist interactions

---
+++ react/src/workshop/ChecklistFlow.test.js
@@ -0,0 +1,5 @@
+import {screen,fireEvent,within} from '@testing-library/react';
+import {renderWorkshop,storedWorkspace,changeRoute,openTask} from './testHelpers';
+
+test('adds acceptance work and toggles completion by its accessible label',async()=>{const {user}=renderWorkshop('#/tasks/T-103');await user.type(screen.getByLabelText('New checklist item'),'Review empty queue chart');await user.click(screen.getByRole('button',{name:'Add item'}));const checkbox=screen.getByRole('checkbox',{name:'Review empty queue chart'});expect(checkbox).not.toBeChecked();await user.click(checkbox);expect(checkbox).toBeChecked();expect(screen.getByLabelText('New checklist item')).toHaveValue('');});
+test('completed work explains disabled checklist editing',()=>{renderWorkshop('#/tasks/T-106');expect(screen.getByRole('checkbox',{name:'Acceptance criteria reviewed'})).toBeDisabled();expect(screen.getByRole('button',{name:'Add item'})).toBeDisabled();});
