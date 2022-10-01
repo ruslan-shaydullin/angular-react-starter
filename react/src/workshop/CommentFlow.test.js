6c873b83521d23d3a67f2b8e5c6fd2ec74564e17
Ruslan Shaydullin <shaydullin.r.d@outlook.com>
2022-10-01 18:53:51 +0500

    Test React comment submission

---
+++ react/src/workshop/CommentFlow.test.js
@@ -0,0 +1,5 @@
+import {screen,fireEvent,within} from '@testing-library/react';
+import {renderWorkshop,storedWorkspace,changeRoute,openTask} from './testHelpers';
+
+test('comments retain literal markup as text and clear only after success',async()=>{const {user}=renderWorkshop('#/tasks/T-103');await user.type(screen.getByLabelText('New comment'),'<b>Review evidence</b>');await user.click(screen.getByRole('button',{name:'Add comment'}));expect(screen.getByText('<b>Review evidence</b>')).toBeInTheDocument();expect(screen.getByLabelText('New comment')).toHaveValue('');expect(storedWorkspace().tasks[2].comments).toHaveLength(1);});
+test('empty comment produces a visible actionable error',async()=>{const {user}=renderWorkshop('#/tasks/T-103');await user.click(screen.getByRole('button',{name:'Add comment'}));expect(screen.getByRole('alert')).toHaveTextContent('Comments must contain');});
