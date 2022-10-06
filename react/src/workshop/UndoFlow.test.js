080defd4738220cf4ba76baedcb197fab88083a3
Ruslan Shaydullin <shaydullin.r.d@outlook.com>
2022-10-06 18:50:53 +0500

    Test React undo restoration

---
+++ react/src/workshop/UndoFlow.test.js
@@ -0,0 +1,5 @@
+import {screen,fireEvent,within} from '@testing-library/react';
+import {renderWorkshop,storedWorkspace,changeRoute,openTask} from './testHelpers';
+
+test('undo and redo restore visible task status and persistence',async()=>{const {user}=renderWorkshop('#/tasks/T-103');expect(screen.getByRole('button',{name:'Undo change'})).toBeDisabled();await user.selectOptions(screen.getByLabelText('Task status'),'doing');await user.click(screen.getByRole('button',{name:'Undo change'}));expect(screen.getByLabelText('Task status')).toHaveValue('ready');expect(storedWorkspace().tasks[2].status).toBe('ready');await user.click(screen.getByRole('button',{name:'Redo change'}));expect(screen.getByLabelText('Task status')).toHaveValue('doing');});
+test('display preferences participate in undo history',async()=>{const {user}=renderWorkshop('#/settings');await user.selectOptions(screen.getByLabelText('Table density'),'compact');await user.click(screen.getByRole('button',{name:'Undo change'}));expect(screen.getByLabelText('Table density')).toHaveValue('comfortable');});
